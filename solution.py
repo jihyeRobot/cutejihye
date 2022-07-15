425ad07f56547bd32f9bd2076f2ddfc69b8f8f25
robot <chcolate1004@naver.com>
2022-07-15 22:17:01 +0900

    K번째 문제 못품

---
+++ solution.py
@@ -0,0 +1,13 @@
+#array 100개 생성
+#임시로 숫자 n개를 입력받고, 배열을 받으면 순서대로 나열
+#i번째 부터 j까지 숫자를 자르고 정렬
+#k 번째 수
+
+def solution(array, commands):
+    array=[list(map(int, input().split())) for_in range(n)]
+    
+    for i in range(100):
+    if:
+            
+    answer = []
+    return answer
